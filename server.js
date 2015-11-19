bd6c659eb8a229a1d9c58f6382d823c8ca931ab9
Ken Lee <kdblitz@gmail.com>
2015-11-19 12:27:52 +0800

    Added browserSync with express.

--- server.js
+++ server.js
@@ -2,10 +2,17 @@
 
 var express = require('express'),
     app = express(),
+    browserSync = require('browser-sync'),
     PORT = process.env.PORT || 8080;
 
 require('./backend/routes')(app);
 
-app.listen(PORT, function() {
-    console.log('Server running on port: ' + PORT);
-});
+app.listen(PORT, listening);
+
+function listening() {
+  console.log('Server running on port: ' + PORT);
+  browserSync({
+    proxy: 'localhost:' + PORT,
+    files: ['app/**/*.{js,css,html}']
+  });
+}
