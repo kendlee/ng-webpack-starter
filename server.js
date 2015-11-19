792f2df093b05107ba598326f00230bc7f9ce477
Ken Lee <kdblitz@gmail.com>
2015-11-19 14:54:38 +0800

    Nodemon options included.

--- server.js
+++ server.js
@@ -3,7 +3,8 @@
 var express = require('express'),
     app = express(),
     browserSync = require('browser-sync'),
-    PORT = process.env.PORT || 8080;
+    PORT = process.env.PORT || 8080,
+    BROWSERSYNC_PORT = process.env.BROWSERSYNC_PORT || 3000;
 
 require('./backend/routes')(app);
 
@@ -12,10 +13,10 @@ app.listen(PORT, listening);
 function listening() {
   browserSync.init({
     open: false,
-    port: 3000,
+    port: BROWSERSYNC_PORT,
     proxy: 'localhost:' + PORT,
     files: ['app/**/*.{js,css,html}']
   }, function() {
-    console.log('Browser sync running on port: ' + 3000);
+    console.log('Browser sync running on port: ' + BROWSERSYNC_PORT);
   });
 }
